eddb843b7ee906e13c7e7744db2708e9cce30dcc
ColinRoye <croye97@gmail.com>
2019-12-02 17:22:22 -0500

    adding caching

--- src/routes.js
+++ src/routes.js
@@ -6,110 +6,119 @@ const env = require("./env");
 const service = require("./services");
 const axios = require("axios");
 
+var apicache = require('apicache')
+var cache = apicache.middleware
 
+apicache.clear()
 
-let getPosts =      async(username, limit)=>{
-     if(!limit){
-          limit = 50
-     }
-     if(limit > 200){
-          limit = 200;
-     }
-     debug.log("url : " +  env.baseUrl + "/items/" + username + "/" + limit)
+let getPosts = async (username, limit) => {
+  if (!limit) {
+    limit = 50
+  }
+  if (limit > 200) {
+    limit = 200;
+  }
+  debug.log("url : " + env.baseUrl + "/items/" + username + "/" + limit)
 
-     let url = env.baseUrl + "/items/" + username + "/" + limit
-     debug.log("LIMIT IN GET POSTS: " + limit);
-     return axios.get(url);
+  let url = env.baseUrl + "/items/" + username + "/" + limit
+  debug.log("LIMIT IN GET POSTS: " + limit);
+  return axios.get(url);
 }
 
 
-router.get('/user/:username/followers', async (req, res, next)=>{
-     debug.log("INPUT: /user/:username/followers " + JSON.stringify(req.params))
-     let followers = (await service.getOrCreateUserByUsername(req.params.username, "arr")).user.followers
-     let temp = [];
-     if(req.query.limit == undefined){
-          req.query.limit = 50
-     }
-     if(req.query.limit > 200){
-          req.query.limit = 200
-     }
-     for(let i = 0; i < req.query.limit &&  i < followers.length;i++){
-          temp.push(followers[i]);
-     }
-     res.send({followers: temp, status:"OK"})
+router.get('/user/:username/followers', cache('2 minutes'), async (req, res, next) => {
+  debug.log("INPUT: /user/:username/followers " + JSON.stringify(req.params))
+  let followers = (await service.getOrCreateUserByUsername(req.params.username, "arr")).user.followers
+  let temp = [];
+  if (req.query.limit == undefined) {
+    req.query.limit = 50
+  }
+  if (req.query.limit > 200) {
+    req.query.limit = 200
+  }
+  for (let i = 0; i < req.query.limit && i < followers.length; i++) {
+    temp.push(followers[i]);
+  }
+  res.send({
+    followers: temp,
+    status: "OK"
+  })
 });
-router.get('/user/:username/following', async (req, res, next)=>{
-     debug.log("INPUT: /user/:username/following " + JSON.stringify(req.params))
-     let following = (await service.getOrCreateUserByUsername(req.params.username, "arr")).user.following
-     let temp = [];
-     if(req.query.limit == undefined){
-          req.query.limit = 50
-     }
-     if(req.query.limit > 200){
-          req.query.limit = 200
-     }
-     for(let i = 0; i < req.query.limit && i < following.length;i++){
-          temp.push(following[i]);
-     }
-     res.send({following: temp, status:"OK"})
+router.get('/user/:username/following', cache('2 minutes'), async (req, res, next) => {
+  debug.log("INPUT: /user/:username/following " + JSON.stringify(req.params))
+  let following = (await service.getOrCreateUserByUsername(req.params.username, "arr")).user.following
+  let temp = [];
+  if (req.query.limit == undefined) {
+    req.query.limit = 50
+  }
+  if (req.query.limit > 200) {
+    req.query.limit = 200
+  }
+  for (let i = 0; i < req.query.limit && i < following.length; i++) {
+    temp.push(following[i]);
+  }
+  res.send({
+    following: temp,
+    status: "OK"
+  })
 });
-router.get('/user/:username', async (req, res, next)=>{
-     debug.log("INPUT: /user/:username: " + JSON.stringify(req.params));
-     //debug.log("OUTPUT:" + (await service.getOrCreateUserByUsername(req.params.username)));
-     console.log("RQ USER/USERNAME: " + JSON.stringify(req.body));
-     let ret = ((await service.getOrCreateUserByUsername(req.params.username)));
-     console.log("RET USER/USERNAME: " + JSON.stringify(ret)); 
-     if(ret.status == env.statusError){
-        ret.status.error = "anything"
-        res.status("400").send(ret)
-     } else{
-       res.send(ret);
-     }
+router.get('/user/:username', cache('2 minutes'), async (req, res, next) => {
+  debug.log("INPUT: /user/:username: " + JSON.stringify(req.params));
+  //debug.log("OUTPUT:" + (await service.getOrCreateUserByUsername(req.params.username)));
+  console.log("RQ USER/USERNAME: " + JSON.stringify(req.body));
+  let ret = ((await service.getOrCreateUserByUsername(req.params.username)));
+  console.log("RET USER/USERNAME: " + JSON.stringify(ret));
+  if (ret.status == env.statusError) {
+    ret.status.error = "anything"
+    res.status("400").send(ret)
+  } else {
+    res.send(ret);
+  }
 });
-router.get('/user/:username/posts', async (req, res, next)=>{
-     debug.log("INPUT: /user/:username/posts" + JSON.stringify(req.params))
-     debug.log("LIMIT: " + req.query.limit)
-     res.send((await getPosts(req.params.username,req.query.limit)).data);
+router.get('/user/:username/posts', cache('2 minutes'), async (req, res, next) => {
+  debug.log("INPUT: /user/:username/posts" + JSON.stringify(req.params))
+  debug.log("LIMIT: " + req.query.limit)
+  res.send((await getPosts(req.params.username, req.query.limit)).data);
 });
 
 
-router.post('/follow', async (req, res, next)=>{
-     // console.log(collection.find({email: 'test@test.cccc'}))
-     debug.log("INPUT: FOLLOW_ROUTE: /follow" + JSON.stringify(req.body))
+router.post('/follow', async (req, res, next) => {
+  // console.log(collection.find({email: 'test@test.cccc'}))
+  debug.log("INPUT: FOLLOW_ROUTE: /follow" + JSON.stringify(req.body))
 
-     debug.log("FOLLOW_ROUTE: top")
-     let args = req.body;
-     let ret = {};
-     let user = req.cookies['auth'];
-     let followUser = args.username;
-     if(user){
-          debug.log("FOLLOW_ROUTE: args.follow === undefined " + args.follow === undefined);
-          debug.log("FOLLOW_ROUTE: args.follow === true " + args.follow === true);
-          debug.log("FOLLOW_ROUTE: args.follow"+args.follow);
+  debug.log("FOLLOW_ROUTE: top")
+  let args = req.body;
+  let ret = {};
+  let user = req.cookies['auth'];
+  let followUser = args.username;
+  if (user) {
+    debug.log("FOLLOW_ROUTE: args.follow === undefined " + args.follow === undefined);
+    debug.log("FOLLOW_ROUTE: args.follow === true " + args.follow === true);
+    debug.log("FOLLOW_ROUTE: args.follow" + args.follow);
 
-          if(args.follow === undefined || args.follow === true){
-               debug.log("FOLLOW_ROUTE: FOLLOW")
-               ret = await service.follow(user, followUser, true);
-               debug.logIt("finised follow")
-          }else{
-               debug.log("FOLLOW_ROUTE: UNFOLLOWING")
-               ret = await service.follow(user, followUser, false);
-               debug.logIt("finised unfollow")
+    if (args.follow === undefined || args.follow === true) {
+      debug.log("FOLLOW_ROUTE: FOLLOW")
+      ret = await service.follow(user, followUser, true);
+      debug.logIt("finised follow")
+    } else {
+      debug.log("FOLLOW_ROUTE: UNFOLLOWING")
+      ret = await service.follow(user, followUser, false);
+      debug.logIt("finised unfollow")
 
-          }
-     }else{
-          debug.log("FOLLOW_ROUTE: user undefined")
+    }
+  } else {
+    debug.log("FOLLOW_ROUTE: user undefined")
 
-          ret.status = env.statusError;
-     }
-     debug.log("test");
-     //res.send(ret)
-     if(ret.status == env.statusError){
-        ret.status.error = "anything"
-        res.status("400").send(ret.status)
-     } else{
-       res.send(ret.status);
-     }
+    ret.status = env.statusError;
+  }
+  debug.log("test");
+  //res.send(ret)
+  if (ret.status == env.statusError) {
+    ret.status.error = "anything"
+    res.status("400").send(ret.status)
+  } else {
+    res.send(ret.status);
+  }
 });
 
 
